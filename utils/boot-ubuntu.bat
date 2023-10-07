1cd311b0b1c3dfb9606eb7ba679da3532e756954
k1z3 <koizumi.takahiro.qg@alumni.tsukuba.ac.jp>
2023-10-08 01:35:26 +0900

    [update] utils script for windows

--- utils/boot-ubuntu.bat
+++ utils/boot-ubuntu.bat
@@ -1,4 +1,12 @@
+@echo off
+
 chcp 65001
 cd /d "%~dp0/.."
 
 wsl -d Ubuntu-22.04 sh -c "sed -i 's/\r//' ./main.sh && ./main.sh ubuntu"
+
+if %ERRORLEVEL% == 0 (
+    timeout /nobreak 5
+) else (
+    pause
+)
